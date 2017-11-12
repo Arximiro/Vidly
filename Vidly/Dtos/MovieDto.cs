c04f0a4d8787abbbae71596c3ac649841bce2322
David Ridgley <arximiro@outlook.com>
2017-11-12 15:23:39 -0500

    Implemented the ability to handle CRUD operations using Web API.

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; }
+
+        
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+
+        
+        public byte GenreId { get; set; }
+    }
+}
